902207652b6bcd535f08cba51e3cfea365172dd1
Lee Ye Seo <rosefor2ne@gmail.com>
2025-09-22 11:53:33 +0900

    feat: NextAuth.js API 동적 라우트 / SessionProvider 추가

---
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+import AppleProvider from "next-auth/providers/apple";
+
+const handler = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID || "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
+    }),
+    AppleProvider({
+      clientId: process.env.APPLE_CLIENT_ID || "",
+      clientSecret: process.env.APPLE_CLIENT_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/signin",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // @ts-expect-error accessToken
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
