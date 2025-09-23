ddb467cf258e7c2f673131f5ef91f58ff9e679f4
yeum-sk <sang.kwon.yeum@gmail.com>
2025-09-23 22:04:28 +0900

    SafeArea 값 webView injection

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,8 +1,53 @@
 import { WebView } from "react-native-webview";
 import { StyleSheet } from "react-native";
+import {
+  type EdgeInsets,
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { useRef, useEffect, useCallback } from "react";
+
+function Container() {
+  const insets = useSafeAreaInsets();
+  const webViewRef = useRef<WebView>(null);
+
+  const injectedJavaScript = `
+    window.safeAreaInsets = ${JSON.stringify(insets)};
+    window.dispatchEvent(new CustomEvent('SafeAreaInsets', { detail: window.safeAreaInsets }));
+    true;
+  `;
+
+  const pushInsets = useCallback((i: EdgeInsets) => {
+    const script = `
+      window.safeAreaInsets = ${JSON.stringify(i)};
+      window.dispatchEvent(new CustomEvent('SafeAreaInsets', { detail: window.safeAreaInsets }));
+      true;
+    `;
+
+    webViewRef.current?.injectJavaScript(script);
+  }, []);
+
+  useEffect(() => {
+    if (webViewRef.current) pushInsets(insets);
+  }, [insets, pushInsets]);
+
+  return (
+    <WebView
+      ref={webViewRef}
+      style={styles.container}
+      source={{ uri: "http://localhost:3000" }}
+      injectedJavaScript={injectedJavaScript}
+      javaScriptEnabled={true}
+    />
+  );
+}
 
 export default function App() {
-  return <WebView style={styles.container} source={{ uri: "https://www.naver.com" }} />;
+  return (
+    <SafeAreaProvider>
+      <Container />
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
